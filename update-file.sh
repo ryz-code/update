c919a45232af831d958fd09a81bef61a77058efc
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-21 18:09:33 +0700

    Create update-file.sh

---
+++ update-file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/nusantara/theme/color.conf)
+export NC="\e[0m"
+export YELLOW='\033[0;33m';
+export RED="\033[0;31m" 
+export COLOR1="$(cat /etc/nusantara/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+export COLBG1="$(cat /etc/nusantara/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
+###########- END COLOR CODE -##########
+
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Remove Old Script"
+#rm /root/install-up.sh
+
+sleep 2
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Downloading New Script"
+#wget -q -O /usr/bin/FILENAME "https://raw.githubusercontent.com/ryz-code/update/main/update_file/FILENAME" && chmod +x /usr/bin/FILENAME
+wget https://raw.githubusercontent.com/ryz-code/update/main/update-file/lolcat.sh && chmod +x lolcat.sh && ./lolcat.sh
+
+sleep 2
+#echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Download Changelog File"
+#wget -q -O /root/changelog.txt "https://raw.githubusercontent.com/ryz-code/update/master/update_file/changelog.txt" && chmod +x /root/changelog.txt
+#echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Read Changelog? ./root/changelog.txt"
+sleep 2
