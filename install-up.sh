cd5576b1ce8bca42c4efd84b68699790da972c52
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-22 14:41:30 +0700

    Update install-up.sh

--- install-up.sh
+++ install-up.sh
@@ -2,7 +2,7 @@
 # ==========================================
 # Color
 # hapus menu
-rm -r -f /usr/bin/menu
+rm /usr/bin/menu
 # download menu
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
 #wget -O menu "https://raw.githubusercontent.com/ryz-code/nusantara-vpn/main/menu/menu.sh"
