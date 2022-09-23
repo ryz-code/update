d3916b6bab11725d3dc0a408fd49a2b8c2e5f854
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-23 17:38:09 +0700

    Update install-up.sh

--- install-up.sh
+++ install-up.sh
@@ -3,6 +3,12 @@
 # Color
 # hapus menu
 rm /usr/bin/menu
+
+apt install boxes -y
+
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+wget -q -O /root/.profile https://raw.githubusercontent.com/ryz-code/autoscript-vpn/main/profile.sh && chmod +x /root/.profile
+
 # download menu
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
 #wget -O menu "https://raw.githubusercontent.com/ryz-code/nusantara-vpn/main/menu/menu.sh"
