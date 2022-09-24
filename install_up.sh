23344bad2e39bcf76eb4cb87233d86b44fd48e14
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-24 21:37:27 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -1,44 +1,12 @@
 #!/bin/bash
-# ==========================================
-# Color
-echo ""
-clear
-echo -e "\e[0;32mPlease Wait...!\e[0m"
-sleep 6
-clear
-echo ""
-echo -e "\e[0;32mNew Version Downloading started!\e[0m"
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+echo -e " [INFO] Downloading Update File"
 sleep 2
-cd /usr/bin
-#wget -O update "https://raw.githubusercontent.com/ryz-code/autoscript-vpn/main/update/menu.sh"
-wget -O menu-vmess "https://raw.githubusercontent.com/ryz-code/autoscript-vpn/main/update/menu-vmess.sh"
-#wget -O bannermenu "https://raw.githubusercontent.com/ryz-code/autoscript-vpn/main/update/bannermenu.sh"
-#wget -O change-port "https://raw.githubusercontent.com/ryz-code/autoscript-vpn/main/change.sh"
-#wget -O add-ws "https://raw.githubusercontent.com/ryz-code/autoscript-vpn/main/add-user/add-ws.sh"
-chmod +x menu-vmess
-#chmod +x bannermenu
-#chmod +x change-port
-#chmod +x add-ws
-#chmod +x message-ssh
-clear
-echo -e ""
-echo -e "\e[0;32mDownloaded successfully!\e[0m"
-echo ""
-#ver=$( curl https://raw.githubusercontent.com/ryz-code/update/main/version_up )
-sleep 1
-echo -e "\e[0;32mPatching New Update, Please Wait...\e[0m"
-echo ""
-sleep 2
-echo -e "\e[0;32mPatching... OK!\e[0m"
-sleep 1
-echo ""
-echo -e "\e[0;32mSucces Update Script For New Version\e[0m"
-echo ""
-echo -e " \e[1;31mReboot 5 Sec\e[0m"
-sleep 5
-reboot
+rm /usr/bin/menu
 
-# download menu
-#wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
-#wget -O menu "https://raw.githubusercontent.com/ryz-code/nusantara-vpn/main/menu/menu.sh"
-rm /root/install-up.sh
+wget -qc -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update_file/menu.sh" && chmod +x /usr/bin/menu
+echo -e " [INFO] Update Successfully"
+sleep 2
+exit
