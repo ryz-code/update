fcec6cec3d0fc5216eeccc9f5c3470702e5d47ce
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-25 00:14:37 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -2,11 +2,20 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-echo -e " [INFO] Downloading Update File"
+colornow=$(cat /etc/scvpn/theme/color.conf)
+export NC="\e[0m"
+export YELLOW='\033[0;33m';
+export RED="\033[0;31m" 
+export COLOR1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+export COLBG1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
+###########- END COLOR CODE -##########
+
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Remove Old Script"
 rm /usr/bin/menu
 sleep 2
+echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Downloading New Script"
 
-wget -qc "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x menu.sh
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x menu.sh
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
