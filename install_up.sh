4b40b5eb75813259e4323c892f98063a4fcb1e88
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-25 00:10:12 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -3,9 +3,10 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 echo -e " [INFO] Downloading Update File"
+rm /usr/bin/menu
 sleep 2
 
-wget -qc "https://raw.githubusercontent.com/ryz-code/update/main/update-file/ins-xray.sh" && chmod +x ins-xray.sh && ./ins-xray.sh
+wget -qc "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x menu.sh
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
