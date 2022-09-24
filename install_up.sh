bf86114d1b13ac61fd9f942582e95f8e43ff5a96
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-24 21:38:28 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -6,7 +6,7 @@ echo -e " [INFO] Downloading Update File"
 sleep 2
 rm /usr/bin/menu
 
-wget -qc -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update_file/menu.sh" && chmod +x /usr/bin/menu
+wget -qc -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
