b12960491b7966f36d9a7f5cd4776c2898aa923c
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-27 23:02:29 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -2,28 +2,30 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 
-echo "1;33m" > /etc/banner
-echo "30m" > /etc/box
-echo "1;33m" > /etc/line
-echo "1;34m" > /etc/text
-echo "1;36m" > /etc/below
-echo "47m" > /etc/back
-echo "1;37m" > /etc/number
-echo 3d > /usr/bin/test
-clear
 echo -e "[INFO] Remove Old Script"
 rm /usr/bin/menu
-rm /etc/issue.net
 sleep 2
-echo -e " [INFO]nDownloading New Script"
+echo -e " [INFO] Downloading New Script"
+apt-get install ruby -y
 
-# banner /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/ryz-code/update/main/update-file/banner/bannerssh.conf"
-echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
-sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
+# install lolcat
+wget https://github.com/busyloop/lolcat/archive/master.zip
+unzip master.zip
+rm -f master.zip
+cd lolcat-master/bin
+gem install lolcat
 
-#Bannerku menu
-wget -O /usr/bin/bannerku "https://raw.githubusercontent.com/ryz-code/update/main/update-file/bannerku" && chmod +x /usr/bin/bannerku
+# install figlet
+apt-get install figlet 
+
+# Install figlet ascii
+sudo apt-get install figlet
+git clone https://github.com/busyloop/lolcat
+cd lolcat/bin && gem install lolcat
+cd /usr/share
+git clone https://github.com/xero/figlet-fonts
+mv figlet-fonts/* figlet && rm –rf figlet-fonts
+
 wget -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
 echo -e " [INFO] Update Successfully"
 sleep 2
