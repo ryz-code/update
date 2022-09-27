06b718abdf980f990bd382a22953b46101f8cd3e
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-27 23:11:58 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -6,27 +6,8 @@ echo -e "[INFO] Remove Old Script"
 rm /usr/bin/menu
 sleep 2
 echo -e " [INFO] Downloading New Script"
-apt-get install ruby -y
-
-# install lolcat
-wget https://github.com/busyloop/lolcat/archive/master.zip
-unzip master.zip
-rm -f master.zip
-cd lolcat-master/bin
-gem install lolcat
-
-# install figlet
-apt-get install figlet 
-
-# Install figlet ascii
-sudo apt-get install figlet
-git clone https://github.com/busyloop/lolcat
-cd lolcat/bin && gem install lolcat
-cd /usr/share
-git clone https://github.com/xero/figlet-fonts
-mv figlet-fonts/* figlet && rm –rf figlet-fonts
-
 wget -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
+wget https://raw.githubusercontent.com/ryz-code/update/main/update-file/lolcat.sh &&  chmod +x lolcat.sh && ./lolcat.sh
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
