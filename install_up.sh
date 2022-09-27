c9b32f9171935f70f3f34eac8d813ecc6db2f659
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-27 23:18:07 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -7,7 +7,6 @@ rm /usr/bin/menu
 sleep 2
 echo -e " [INFO] Downloading New Script"
 wget -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
-wget https://raw.githubusercontent.com/ryz-code/update/main/update-file/lolcat.sh &&  chmod +x lolcat.sh && ./lolcat.sh
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
