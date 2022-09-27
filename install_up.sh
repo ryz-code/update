8859d1f86b9f239379168086e27d43fb7ff6dd5e
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-28 06:12:31 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-
+rm /usr/bin/menu
 echo -e "[INFO] Remove Old Script"
 
 sleep 2
-cd /usr/bin
 echo -e " [INFO] Downloading New Script"
-wget -qc -O menu-theme "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu-theme.sh"
-chmod +x menu-theme
+wget -qc -O /usr/bin/menu-themes "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu-theme.sh" && chmod +x /usr/bin/menu-themes
+wget -qc -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
 echo -e " [INFO] Update Successfully"
 
 rm install_up.sh
