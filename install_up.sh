528d4e1dc4367950d95828444820ac453fa8a172
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-10-03 17:52:05 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -7,6 +7,7 @@ echo -e "[INFO] Remove Old Script"
 sleep 2
 echo -e " [INFO] Downloading New Script"
 wget -qc -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/update/main/update-file/menu.sh" && chmod +x /usr/bin/menu
+wget -qc -o /opt/.ver https://raw.githubusercontent.com/ryz-store/autoscript-premium/main/ver && chmod +x /opt/.ver
 echo -e " [INFO] Update Successfully"
 
 rm install_up.sh
